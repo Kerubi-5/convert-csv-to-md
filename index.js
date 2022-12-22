710abc0e9e66563c6f998a4fd41a3dc1b3da1f92
John Kim <querobinesj@gmail.com>
2022-12-22 09:30:13 +0800

    create function to convert csv to md

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import fs from "fs"; // File system module
 import Papa from "papaparse"; // CSV parsing module
 import yaml from "js-yaml"; // Converts Javascript Object to valid yaml properties
+import { Parser } from "xml2js";
+import { createInterface } from "readline";
+
+// Create a readline interface
+const r1 = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 function sanitizeFileName(name) {
   const invalidCharRegex = /[\\/:*?"<>|]/g;
@@ -8,54 +16,81 @@ function sanitizeFileName(name) {
   return name.replace(invalidCharRegex, "_");
 }
 
-const csvFileName = "input.csv"; // change the value of the string to your csv file name
-
-// Read the CSV file
-fs.readFile(`input/${csvFileName}`, "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  // Parse the CSV data
-  const csvData = Papa.parse(data, {
-    header: true, // Use the first row as the header
+function createMarkdownFile(frontmatter, content) {
+  const validFrontMatter = yaml.dump(frontmatter, {
+    indent: 2,
+    styles: {
+      categories: "block",
+    },
   });
 
-  // Loop through each row in the CSV data
-  csvData.data.forEach((row) => {
-    // Extract the frontmatter properties and content from the row
-    const frontmatter = {
-      title: row.Title,
-      postStatus: row["Post status"],
-      pubDate: row["Published date"],
-      tags: row.Tags,
-      categories: row.Categories?.split("|"),
-    };
+  const parsedContent = content ?? "";
 
-    const validFrontMatter = yaml.dump(frontmatter, {
-      indent: 2,
-      styles: {
-        categories: "block",
-      },
-    });
+  // Create the Markdown content
+  const markdown = `---\n${validFrontMatter}\n---\n\n${parsedContent}\n`;
 
-    const parsedContent = row.Content ?? "";
+  if (!fs.existsSync("markdown")) fs.mkdirSync("markdown"); // Create the output directory if it doesn't exist
 
-    // Create the Markdown file
-    const markdown = `---\n${validFrontMatter}\n---\n\n${parsedContent}\n`;
+  const fileName = `${process.cwd()}/markdown/${sanitizeFileName(
+    frontmatter.title
+  )}.md`;
 
-    if (!fs.existsSync("markdown")) fs.mkdirSync("markdown"); // Create the output directory if it doesn't exist
+  // Write the Markdown file to disk
+  fs.writeFile(fileName, markdown, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
 
-    const fileName = `${process.cwd()}/markdown/${sanitizeFileName(
-      row.Title
-    )}.md`;
+// Prompt user for the file name
+r1.question(
+  "Enter the name of your file NOTE: (make sure to put it inside the input folder or if it doesn't exist kindly create one): ",
+  (csvFileName) => {
+    console.log("Parsing and reading your files...");
 
-    // Write the Markdown file to disk
-    fs.writeFile(fileName, markdown, (err) => {
+    // Read the file
+    fs.readFile(`input/${csvFileName}`, "utf8", (err, data) => {
       if (err) {
         console.error(err);
+        r1.close();
+        return;
       }
+
+      const fileExtension = csvFileName.match(/\.[^.]+$/)[0];
+
+      if (fileExtension === ".csv") {
+        // Parse the CSV data
+        const csvData = Papa.parse(data, {
+          header: true, // Use the first row as the header
+        });
+
+        // Loop through each row in the CSV data
+        csvData.data.forEach((row) => {
+          // Extract the frontmatter properties and content from the row
+          const frontmatter = {
+            title: row.Title,
+            status: row["Post status"],
+            datePublished: row["Published date"],
+            tags: row.Tags,
+            categories: row.Categories?.split("|"),
+          };
+
+          createMarkdownFile(frontmatter, row.Content);
+        });
+      } else if (fileExtension === ".xml") {
+        const xmlParser = new Parser();
+
+        xmlParser.parseString(data, (err, result) => {});
+      } else {
+        console.log("Your file is neither a csv or xml");
+      }
+
+      console.log(
+        "Finished converting your files take a look at the markdown folder inside your directory"
+      );
+
+      r1.close();
     });
-  });
-});
+  }
+);
